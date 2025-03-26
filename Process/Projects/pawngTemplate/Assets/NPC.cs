2c33544d8964db3ce4c9738ec0e15ecb955a40c8
Arianna <70153178+Arianna19@users.noreply.github.com>
2025-03-26 15:56:20 -0400

    Made a dialogue screen by pressing E
    Added an npc character place holder for the future. Theres a screen displayed when interacted with that displays text gradually and a continue button if theres more text needed

---
+++ Process/Projects/pawngTemplate/Assets/NPC.cs
@@ -0,0 +1,124 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NPC : MonoBehaviour
+{
+    public GameObject dialoguePanel;
+    public TextMeshProUGUI dialogueText;
+    public string[] dialogue;
+    private int index = 0;
+    public GameObject contButton;
+
+    public float wordSpeed = 0.05f; // Default value to avoid 0
+    public bool playerIsClose;
+
+    void Start()
+    {
+        if (dialogueText != null)
+        {
+            dialogueText.text = "";
+        }
+    }
+
+    void Update()
+    {
+        if (playerIsClose && Input.GetKeyDown(KeyCode.E))
+        {
+            if (dialoguePanel != null && !dialoguePanel.activeInHierarchy)
+            {
+                dialoguePanel.SetActive(true);
+                StartCoroutine(Typing());
+            }
+            else if (dialoguePanel != null && dialogueText.text == dialogue[index])
+            {
+                NextLine();
+            }
+        }
+
+        if (dialoguePanel != null && dialoguePanel.activeInHierarchy && Input.GetKeyDown(KeyCode.Q))
+        {
+            RemoveText();
+        }
+
+        if (dialoguePanel != null && dialoguePanel.activeInHierarchy && dialogueText.text == dialogue[index])
+        {
+            if (contButton != null)
+            {
+                contButton.SetActive(true);
+            }
+        }
+    }
+
+    public void RemoveText()
+    {
+        if (dialogueText != null)
+        {
+            dialogueText.text = "";
+        }
+        index = 0;
+
+        if (dialoguePanel != null)
+        {
+            dialoguePanel.SetActive(false);
+        }
+    }
+
+    IEnumerator Typing()
+    {
+        if (dialogue.Length == 0 || dialogueText == null) yield break; // Prevent errors if no dialogue
+
+        dialogueText.text = ""; // Clear text before typing
+        foreach (char letter in dialogue[index].ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(wordSpeed);
+        }
+    }
+
+    public void NextLine()
+    {
+        if (contButton != null)
+        {
+            contButton.SetActive(false);
+        }
+
+        if (index < dialogue.Length - 1)
+        {
+            index++;
+            if (dialogueText != null)
+            {
+                dialogueText.text = "";  // Clear text before typing new line
+            }
+            StartCoroutine(Typing());
+        }
+        else
+        {
+            RemoveText();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Paddle"))  // Make sure Player tag is correct
+        {
+            playerIsClose = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Paddle"))
+        {
+            playerIsClose = false;
+
+            // Only hide if the panel exists and is active
+            if (dialoguePanel != null && dialoguePanel.activeInHierarchy)
+            {
+                dialoguePanel.SetActive(false);
+            }
+        }
+    }
+}
